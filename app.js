3656d1d25a8065d93be8fc4dacfb42b4ed275ddb
rrbyn <robin@DESKTOP-Q79J1H3.localdomain>
2020-05-04 14:24:39 +0300

    added to js file

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
+const shortcuts = {
+    "data": [
+        {
+        "question":"kopeeri",
+        "answer":[17,67]
+        },
+        {
+        "question":"comment",
+        "answer":[17,75,67]
+        },
+        {
+        "question":"kleebi",
+        "answer":[17,86]
+    }
+        
+    ],
+    "meta": {
+        "question": "Mis klahvikombinatsiooni on vaja vajutada, et *",
+        "answers": 5,
+        "title": "Kiirklahvid",
+        "type":"shortcuts"
+        }   
+
+    }
+
 const inputField = document.querySelector("#input-field")
 
 inputField.addEventListener("keydown", event => {
