820971a9b9cd7e0b96d1070612ed165b81c6a66e
Siily15 <arnokosk15@gmal.com>
2020-05-06 13:09:24 +0300

    Add question

--- app.js
+++ app.js
@@ -1,29 +1,32 @@
 const shortcuts = {
     "data": [
         {
-        "question":"kopeeri",
-        "answer":[17,67]
+            "question": "kopeeri",
+            "answer": [17, 67]
         },
         {
-        "question":"comment",
-        "answer":[17,75,67]
+            "question": "comment",
+            "answer": [17, 75, 67]
         },
         {
-        "question":"kleebi",
-        "answer":[17,86]
-    }
-        
+            "question": "kleebi",
+            "answer": [17, 86]
+        }
+
     ],
     "meta": {
         "question": "Mis klahvikombinatsiooni on vaja vajutada, et *",
         "answers": 5,
         "title": "Kiirklahvid",
-        "type":"shortcuts"
-        }   
-
+        "type": "shortcuts"
     }
 
+}
+
 const inputField = document.querySelector("#input-field")
+const questionDiv = document.querySelector("#question")
+
+questionDiv.innerText = shortcuts.data[0].question
 
 inputField.addEventListener("keydown", event => {
     console.log(event)
